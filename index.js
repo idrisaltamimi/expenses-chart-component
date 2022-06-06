2da0285a42cc27bd68e21ac52be704dc6fce0634
idrisaltamimi <97464844+idrisaltamimi@users.noreply.github.com>
2022-06-06 14:46:07 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-const jsonFile = async () => {
-   const res = await fetch("./data.json")
-   return res.json()
-}
-const jsonData = await jsonFile()
-
+// const jsonFile = async () => {
+//    const res = await fetch("./data.json")
+//    return res.json()
+// }
+// const jsonData = await jsonFile()
+const jsonData = [
+   {
+      "day": "mon",
+      "amount": 17.45
+   },
+   {
+      "day": "tue",
+      "amount": 34.91
+   },
+   {
+      "day": "wed",
+      "amount": 52.36
+   },
+   {
+      "day": "thu",
+      "amount": 31.07
+   },
+   {
+      "day": "fri",
+      "amount": 23.39
+   },
+   {
+      "day": "sat",
+      "amount": 43.28
+   },
+   {
+      "day": "sun",
+      "amount": 25.48
+   }
+]
 const myChart = document.getElementById("chart")
 const days = jsonData.map(i => i.day)
 const data = jsonData.map(i => i.amount)
